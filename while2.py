8f9fa0d5bf6fadbb77e0d90857b675c5d0775649
Anat <an.an19930420@gmail.com>
2019-06-08 12:03:50 +0900

    add new file

---
+++ while2.py
@@ -0,0 +1,12 @@
+x = 1
+while x <= 3 :
+	password = input ('please input your password:')
+	if password == '5418':
+		print ('successfully login')
+	else:
+		print ('you have', 3-x , 'times chance')
+	x = x + 1
+
+
+
+
